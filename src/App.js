70dc27740b7eff94e410b20469f5782da0b5cf9d
l1ttlestar <michelle020517@gmail.com>
2022-04-18 00:23:24 +0900

    4장 04-2까지 완성된 소스코드

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+function Idol({name, picture}) {
+  return (
+    <div>
+      <h2>I like {name}</h2>;
+      <img src={picture} />
+    </div>
+  );
+}
+
+const IdolILike = [
+{
+  name:'nct',
+  image: 'https://img.wowtv.co.kr/wowtv_news/dnrs/20220211/2022021109080808018d3244b4fed58141237161.jpg'
+},
+{
+  name:'red velvet',
+  image:'https://image.ytn.co.kr/general/jpg/2022/0321/202203211441229279_d.jpg'
+},
+{
+  name:'seventeen',
+  image:'https://talkimg.imbc.com/TVianUpload/tvian/TViews/image/2022/04/12/687d0a98-4dcf-4ae7-b8e3-57f4443acbd2.jpg'
+},
+{
+  name:'oh my girl',
+  image:'https://cdn.mhnse.com/news/photo/202204/100312_83181_2544.jpg'
+},
+{
+  name:'aespa',
+  image:'https://img.vogue.co.kr/vogue/2021/10/style_617668d772d73-930x930.jpeg'
+},
+{
+  name:'IU',
+  image:'https://pbs.twimg.com/profile_images/1374979417915547648/vKspl9Et_400x400.jpg'
+}
+
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    {IdolILike.map(group =>  <Idol key={group.name} name={group.name} picture={group.image} />)}
+  </div>
   );
 }
 
